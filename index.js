b60b74f389868347a6d021a1c96ffb014c5989b0
LokeshV790 <lokeshvazirani29@gmail.com>
2023-08-14 12:44:12 +0530

    Booking: Connect to MongoDB
    Signed-off-by: LokeshV790 <lokeshvazirani29@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-import express from "express"
-const app = express()
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+const app = express();
+dotenv.config();
 
-app.listen(8800, ()=>{
-    console.log("connected to backend..on port : 8800")
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB.");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("Mongodb Disconnected.")
 })
+
+mongoose.connection.on("connected", ()=>{
+    console.log("Mongodb Connected.")
+})
+
+app.listen(8800, () => {
+  console.log("Connected to backend..on port : 8800");
+  connect();
+});
