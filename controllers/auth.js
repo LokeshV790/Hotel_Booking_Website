e252a900af4e62af220a4e4fdd8e7c776a8eea23
LokeshV790 <lokeshvazirani29@gmail.com>
2023-08-15 10:04:16 +0530

    Booking: Implement Auth with password security
    Signed-off-by: LokeshV790 <lokeshvazirani29@gmail.com>

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,29 @@
-import User from "../models/Users.js"
+import User from "../models/Users.js";
+import bcrypt from "bcryptjs";
 
-export const register = async (req,res,next)=>{
+export const register = async (req, res, next) => {
     try {
-        const newUser = new User({
-            username:req.body.username,
-            email:req.body.email,
-            password:req.body.password,
-        })
+        const { username, email, password } = req.body;
 
-        await newUser.save()
-        res.status(200).send("User has been created.")
+        console.log("Received registration request for:", username, email);
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+
+        console.log("Generated hash:", hash);
+
+        const newUser = new User({
+            username,
+            email,
+            password: hash,
+        });
+
+        await newUser.save();
+        console.log("User has been created:", newUser);
+
+        res.status(200).send("User has been created.");
     } catch (error) {
-        next(error)
+        console.error("Error while registering user:", error);
+        next(error);
     }
-}
+};
--- models/Users.js
+++ models/Users.js
@@ -5,16 +5,15 @@ const userSchema = new mongoose.Schema({
     username:{
         type: String,
         required:true,
-        unique:true
     },
     email:{
         type:String,
         require:true,
-        unique:true
+        unique:true,
     },
     password:{
         type:String,
-        required:true
+        required:true,
     },
     isAdmin:{
         type: Boolean,
